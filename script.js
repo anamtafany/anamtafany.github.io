f40bf745fde75f6aa466020f00b9cf6925361355
Khoirul Anam <anamtpnt@gmail.com>
2023-09-02 19:30:30 +0700

    commit pertama

---
+++ script.js
@@ -0,0 +1,6 @@
+const bgJudul = document.querySelector('.bg');
+
+bgJudul.addEventListener('click', function(){
+    bgJudul.style.backgroundColor = 'orange';
+    // bgJudul.classList.toggle('bg');
+})
